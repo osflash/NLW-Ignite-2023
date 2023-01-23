7d404f975302492c62b56d4245b11ea7497a7889
OsFlash <felipedasilvatomaz1996@gmail.com>
2023-01-23 02:14:36 -0300

    test

--- nextjs/src/services/prisma.ts
+++ nextjs/src/services/prisma.ts
@@ -4,6 +4,8 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
+console.log(process.env.DATABASE_URL_BACKUP)
+
 export const prisma = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
