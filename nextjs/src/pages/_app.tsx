6ba552b79721d9e7f6b446abce68d9f7cf36e679
OsFlash <felipedasilvatomaz1996@gmail.com>
2023-01-23 11:11:47 -0300

    add pwa and test react-onesignal

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import '~/lib/dayjs'
 
 import { Session } from 'next-auth'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Script from 'next/script'
 
 import Providers from '~/components/Providers'
 
@@ -12,9 +14,37 @@ interface MyAppProps extends AppProps {
 
 const App: React.FC<MyAppProps> = ({ Component, pageProps, session }) => {
   return (
-    <Providers session={session}>
-      <Component {...pageProps} />
-    </Providers>
+    <>
+      <Head>
+        <title>NLW 2023</title>
+        <meta name="application-name" content="Habit - NLW Ignite 2023" />
+        <meta name="description" content="NLW 2023" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#4c1d95" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" />
+      <Providers session={session}>
+        <Component {...pageProps} />
+      </Providers>
+    </>
   )
 }
 
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import OneSignal from 'react-onesignal'
 
 import { useSession } from 'next-auth/react'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 import { Header } from '~/components/Header'
@@ -14,30 +14,42 @@ const Home: React.FC = () => {
   const user = session?.user.id
   const queryUser = router.query['user']
 
-  // http://localhost:3000/?user=cld8icsth0000mf0913bc2gtu
-
   useEffect(() => {
     if (!user || !router.isReady || queryUser) return
 
     router.push(`?user=${user}`)
   }, [queryUser, router, user])
 
-  return (
-    <>
-      <Head>
-        <title>NLW 2023</title>
-        <meta name="description" content="NLW 2023" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  useEffect(() => {
+    const runOneSignal = async () => {
+      OneSignal.init({
+        appId: process.env.NEXT_PUBLIC_ONESIGNAL!,
+        allowLocalhostAsSecureOrigin: true
+      })
 
-      <div className="w-screen h-screen flex justify-center items-center">
-        <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-          <Header />
-          <SummaryTable />
-        </div>
+      await OneSignal.showSlidedownPrompt()
+
+      OneSignal.isPushNotificationsEnabled(action => {
+        console.log(action)
+      })
+
+      OneSignal.on('subscriptionChange', isSubscribed => {
+        console.log('O estado da assinatura do usuário agora é:', isSubscribed)
+      })
+
+      OneSignal.on('permissionPromptDisplay', () => {})
+    }
+
+    runOneSignal()
+  }, [])
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
       </div>
-    </>
+    </div>
   )
 }
 
