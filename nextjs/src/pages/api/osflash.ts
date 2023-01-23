ecc4069c3bed8880388258e449894825b712c1d6
OsFlash <felipedasilvatomaz1996@gmail.com>
2023-01-23 02:27:58 -0300

    fix

--- nextjs/src/pages/api/osflash.ts
+++ nextjs/src/pages/api/osflash.ts
@@ -4,5 +4,7 @@ import nc from 'next-connect'
 const handler = nc<NextApiRequest, NextApiResponse>()
 
 handler.get(async (req, res) => {
-  res.status(200).send(process.env.DATABASE_URL_BACKUP)
+  const message = !!process.env.DATABASE_URL_BACKUP
+
+  res.status(200).send(message)
 })
